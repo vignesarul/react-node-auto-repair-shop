8188da534096b6e9f2637827fd618271bfb4e39a
Vignes Aruljothi <vignes.arul@gmail.com>
2017-09-10 09:50:02 +0530

    Added placeholder files

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Routes from 'routes';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import rootReducer from 'reducers';
+import Routes from 'routes';
 import createSagaMiddleware from 'redux-saga';
 import rootSaga from 'redux-sagas';
 import registerServiceWorker from './registerServiceWorker';
---
+++ src/routes/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Routes = () => (<div />);
+
+export default Routes;
