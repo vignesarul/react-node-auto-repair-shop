5238adfdf5b3104699ea70e7b659e233a4f58ce3
Vignes Aruljothi <vignes.arul@gmail.com>
2017-09-24 19:33:45 +0530

    Updated config file

--- src/common/config/config.js
+++ src/common/config/config.js
@@ -4,11 +4,9 @@ module.exports = {
     version: 'v1',
     status: 'test', // test || live
     host: 'http://localhost:3001/api/v1',
-    // host: 'https://desolate-coast-39570.herokuapp.com/api/v1',
   },
   database: {
     uri: 'mongodb://localhost:27017/autorepair',
-    // uri: 'mongodb://devuser:sadfasdfgdf@ds123933.mlab.com:23933/calories',
     table: '',
     validation: { runValidators: true, context: 'query' },
   },
@@ -16,19 +14,15 @@ module.exports = {
     level: '',
   },
   secret: {
-    passwordSalt: '$2a$10$lvh1E1ZVePCsuLSN22jI3e',
-    jwtSignature: 'devAppSecret',
-  },
-  nutritionix: {
-    key: '1bc56afe2aef0fa1e670a6ad30676936',
-    id: '3522a250',
+    passwordSalt: '123',
+    jwtSignature: '123',
   },
   listing: {
     limit: 5,
   },
   mail: {
     sendgrid: {
-      apiKey: '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',
+      apiKey: '',
       templates: {
         newUser: {
           subject: 'Verify your account',
